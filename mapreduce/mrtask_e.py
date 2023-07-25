aea72e7c09dc6604588071a21f49251f2ed07eb6
SharadChoudhury <sharadch27@gmail.com>
2023-07-25 21:33:05 +0530

    task e done

---
+++ mapreduce/mrtask_e.py
@@ -0,0 +1,40 @@
+# Calculate the average tips to revenue ratio (total tips/total revenue) of the drivers for 
+# different pickup locations in sorted format.
+
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+
+class mapreduce(MRJob):
+
+    def steps(self):
+        return [ MRStep(mapper = self.mapper, reducer = self.reducer),
+                    MRStep(reducer = self.sorted_ratios) ]
+
+    def mapper(self, key, value):
+        vals = value.strip().split(',')
+        if vals[0] != 'VendorID':
+            puloc = vals[7]
+            tip = float(vals[13])
+            rev = float(vals[16])
+            yield puloc,(tip, rev)
+
+
+    def reducer(self, puloc, values):
+        totaltip = 0
+        totalrev = 0
+        for tip,rev in values:
+            totaltip += tip
+            totalrev += rev
+        yield None, (totaltip/totalrev, puloc)
+
+
+    def sorted_ratios(self, key, values):
+        values = sorted(values)
+        yield "Pickup location ", " Tip to revenue ratio "
+        for ratio,loc in values:
+            yield loc, round(ratio,2)
+
+
+if __name__ == "__main__":
+    mapreduce.run()
