41762d5defb54f35f13e3e2417fc54065e5751e4
Sharad Choudhury <sharadch27@gmail.com>
2023-08-03 18:43:33 +0530

    batch file edited

--- batch_insert.py
+++ batch_insert.py
@@ -7,7 +7,6 @@ connection = happybase.Connection('localhost', port=9090 ,autoconnect=False)
 def open_connection():
     connection.open()
 
-
 #close the opened connection
 def close_connection():
     connection.close()
@@ -15,27 +14,38 @@ def close_connection():
 
 #get the pointer to a table
 def get_table():
-    open_connection()
-    print(connection.tables())
+    # print(connection.tables())
     table_name = 'taxidata'
     table = connection.table(table_name)
-    close_connection()
     return table
 
 
+def getlastrow(table):
+    # Start a scanner with descending order to get the last row key
+    scanner = table.scan(reverse=True, limit=1)
+    # Get the last row key (if any)
+    last_row_key = next(iter(scanner), [0])
+    # Close the scanner
+    scanner.close()
+    # Extract the row key from the result, if available
+    last_row_key = last_row_key[0]
+    return int(last_row_key)
+
+    
 #batch insert data in events table 
 def batch_insert_data(filename):
-    print("starting batch insert of events")
-    file = open(filename, "r")
-    table = get_table()
     open_connection()
 
-    i= 0
+    file = open(filename, "r")
+    table = get_table()
+    lastrow = getlastrow(table)
+    i = lastrow 
     cols = []
 
+    print("starting batch insert of events")
     with table.batch(batch_size=1000) as b:
         for line in file:
-            if i!=0:
+            if i != lastrow :
                 temp = line.strip().split(",")
                 row_key = str(i).encode()  # Encode row key to bytes
                 for j in range(10):
@@ -44,15 +54,15 @@ def batch_insert_data(filename):
                     b.put(row_key, {b'fare_info:' + cols[k].encode(): temp[k].encode()})
                 print('inserted {}th line'.format(i))
             else:
-                cols = line.strip().split(",")
+                cols = line.strip().split(",")          
             i+=1
-    
 
     file.close()
     print("batch insert done, {} lines inserted".format(i-1))
+    
     close_connection()
 
 
-# insert batch data from march.csv and april.csv
-batch_insert_data('mar.csv')
-batch_insert_data('apr.csv')
+if __name__ == '__main__':
+    batch_insert_data('mar.csv')
+    batch_insert_data('apr.csv')
